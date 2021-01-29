8673077e9f44d33e7574194dc26de568c58121a8
José Chacón <josejch11@gmail.com>
2021-01-29 18:54:10 -0500

    adición de proyecto base

---
+++ eda/josé/dash-earthquake-analysis/index.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from pages import (
+    overview,
+    pricePerformance,
+    portfolioManagement,
+    feesMins,
+    distributions,
+    newsReviews,
+    magnitud)
+# Connect to main app.py file
+from app import app
+import pandas as pd
+import plotly.express as px
+
+# Describe the layout/ UI of the app
+app.layout = html.Div(
+    [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
+)
+app.config.suppress_callback_exceptions = True
+
+
+# Update page
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def display_page(pathname):
+    if pathname == "/dash-financial-report/price-performance":
+        return pricePerformance.create_layout(app)
+    elif pathname == "/dash-financial-report/portfolio-management":
+        return portfolioManagement.create_layout(app)
+    elif pathname == "/dash-financial-report/fees":
+        return feesMins.create_layout(app)
+    elif pathname == "/dash-financial-report/distributions":
+        return distributions.create_layout(app)
+    elif pathname == "/dash-financial-report/news-and-reviews":
+        return newsReviews.create_layout(app)
+    elif pathname == "/dash-financial-report/magnitud":
+        return magnitud.layout
+    elif pathname == "/dash-financial-report/full-view":
+        return (
+            overview.create_layout(app),
+            pricePerformance.create_layout(app),
+            portfolioManagement.create_layout(app),
+            feesMins.create_layout(app),
+            distributions.create_layout(app),
+            newsReviews.create_layout(app),
+            magnitud.layout,
+        )
+    else:
+        return overview.create_layout(app)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
